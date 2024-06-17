1f11b3b056aa8e7d142fb7878826f5f658d5cdd7
Manish <mkc1020304050@gmail.com>
2024-06-17 11:27:20 +0530

    signup/onboarding form

--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -1,20 +1,37 @@
+import Input from "@/components/common/Input";
 import { useStateProvider } from "@/context/StateContext";
 import Image from "next/image";
-import React from "react";
+import React, {useState} from "react";
 
 function onboarding() {
-  const [{userInfo}]= useStateProvider();
+  const [{ userInfo }] = useStateProvider();
   console.log(userInfo);
-  return <div className="bg-panel-header-background h-screen w-screen text-white flex flex-col items-center justify-center  ">
-    <div className="flex items-center justify-center gap-2 ">
-      <Image src = "/home-logo.png" alt="Gossip Arena 2" height={300} width={300} />
-      <span className="text-7xl" >Gossip Arena 2.0</span>
+
+  const [name,setName] = useState(userInfo?.name || "");
+  const [about,setAbout] = useState("");
+  const [image,setImage] = useState("/default_avator.png");
+
+
+  return (
+    <div className="bg-panel-header-background h-screen w-screen text-white flex flex-col items-center justify-center  ">
+      <div className="flex items-center justify-center gap-2 ">
+        <Image
+          src="/home-logo.png"
+          alt="Gossip Arena 2"
+          height={300}
+          width={300}
+        />
+        <span className="text-7xl">Gossip Arena 2.0</span>
+      </div>
+      <h2 className="text-2xl">Create Your Profile</h2>
+      <div className="flex gap-6 mt-6">
+        <div className="flex flex-col items-center justify-center mt-5 gap-6 ">
+          <Input name="Display Name" state={name} setState={setName} label />
+          <Input name="About" state={about} setState={setAbout} label />
+        </div>
+      </div>
     </div>
-    <h2 className="text-2xl">Create Your Profile</h2>
-    <div className="flex gap-6 mt-6">
-      <div className="flex flex-col items-center justify-center mt-5 gap-6 "></div>
-    </div>
-  </div>;
+  );
 }
 
 export default onboarding;

fabb161f05d9d39a83f4f38d663a34965cf7c907
Manish <mkc1020304050@gmail.com>
2024-06-27 03:34:34 +0530

    getAllUsers api

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { onBoardUser, verifyUser } from "../controllers/AuthController.js";
+import { getAllUsers, onBoardUser, verifyUser } from "../controllers/AuthController.js";
 
 
 const router = Router();
 
 router.post("/verify-user",verifyUser);
 router.post("/onboard-user",onBoardUser);
+router.get("/get-contacts",getAllUsers)
 
 export default router;

0d2068a93e1449472c2ed2b2885beb57be5e07e6
Manish <mkc1020304050@gmail.com>
2024-06-27 22:49:13 +0530

    contact component

--- client/src/components/Chatlist/ChatList.jsx
+++ client/src/components/Chatlist/ChatList.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ChatListHeader from "./ChatListHeader";
 import SearchBar from "./SearchBar";
 import List from "./List";
+import { useStateProvider } from "@/context/StateContext";
+import ContactsList from "./ContactsList";
 
 function ChatList() {
-  return <div className="bg-panel-header-background flex flex-col max-h-screen">
-    <ChatListHeader/>
-    <SearchBar/>
-    <List/>
+  const [{ contactsPage }] = useStateProvider();
+  const [pageType, setPageType] = useState("default");
 
-  </div>;
+  useEffect(() => {
+    if (contactsPage) {
+      setPageType("all-contacts");
+    } else {
+      setPageType("default");
+    }
+  }, [contactsPage]);
+  return (
+    <div className="bg-panel-header-background flex flex-col max-h-screen">
+      {pageType === "default" && (
+        <>
+          <ChatListHeader />
+          <SearchBar />
+          <List />
+        </>
+      )}
+      {pageType === "all-contacts" && <ContactsList />}
+    </div>
+  );
 }
 
 export default ChatList;
--- client/src/utils/ApiRoutes.js
+++ client/src/utils/ApiRoutes.js
@@ -4,3 +4,4 @@ const AUTH_ROUTE = `${SERVER_BASE_URL}/api/auth`;
  
 export const VERIFY_USER_ROUTE = `${AUTH_ROUTE}/verify-user`;
 export const ONBOARD_USER_ROUTE = `${AUTH_ROUTE}/onboard-user`;
+export const GET_ALL_CONTACTS =  `${AUTH_ROUTE}/get-contacts`;

165d81cdbb2dda80fb0bb6313cf0fcbf5eed5694
Manish <mkc1020304050@gmail.com>
2024-06-30 12:50:00 +0530

    schema for messages added

--- client/src/components/Chat/MessageBar.jsx
+++ client/src/components/Chat/MessageBar.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
 import { ImAttachement } from "react-icons/im";
 import { BsEmojiSmile } from "react-icons/bs";
 import { MdSend } from "react-icons/md";
 import { FaMicrophone } from "react-icons/fa";
+import { useStateProvider } from "@/context/StateContext";
 
 function MessageBar() {
+  const [{userInfo,currentChatUser},dispatch] = useStateProvider();
+  const [message,setMessage] = useState("");
+  const sendMessage = async() => {
+    alert(message);
+
+
+    setMessage("");
+  }
   return (
     <div className="bg-panel-header-background h-20 px-4 flex items-center gap-6 relative">
       <>
@@ -23,11 +32,13 @@ function MessageBar() {
             type="text"
             placeholder="Type a message"
             className="bg-input-background text-sm focus:oultine-none text-white h-10 rounded-lg px-5 py-4 w-full"
+            onChange={(e)=>setMessage(e.target.value)}
+            value={message}
           />
         </div>
         <div className="flex w-10 items-center justify-center ">
           <button className="">
-            <MdSend className="text-panel-header-icon cursor-pointer text-xl" title="send messages"/>
+            <MdSend className="text-panel-header-icon cursor-pointer text-xl" title="send messages" onClick={sendMessage} />
             {/* <FaMicrophone className="text-panel-header-icon cursor-pointer text-xl" title="Record"/> */}
           </button>
         </div>
